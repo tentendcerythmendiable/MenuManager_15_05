580e1e0914562d86d21cf1e0f2aecfd9cb2f6e80
tentendcerythmendiable <125271192+tentendcerythmendiable@users.noreply.github.com>
2023-05-15 12:28:57 +0200

    Initial commit

---
+++ MenuManager.Ui.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using MenuManager.Ui.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using MenuManager.Core;
+
+namespace MenuManager.Ui.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly MenuManagerDbContext _menuManagerDbContext;
+        
+        public HomeController(MenuManagerDbContext menuManagerDbContext)
+        {
+            _menuManagerDbContext = menuManagerDbContext;
+        }
+        
+        public IActionResult Index()
+        {
+            var products = _menuManagerDbContext.Products.ToList();
+
+            return View(products);
+        }
+
+        
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MenuManager.Ui.WebApp/Controllers/ProductsController.cs
@@ -0,0 +1,113 @@
+﻿using MenuManager.Core;
+using MenuManager.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MenuManager.Ui.WebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly MenuManagerDbContext _menuManagerDbContext;
+
+        public ProductsController(MenuManagerDbContext menuManagerDbContext)
+        {
+            _menuManagerDbContext = menuManagerDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var products = _menuManagerDbContext.Products.ToList();
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+
+            product.CreatedDate = DateTime.Now;
+
+            _menuManagerDbContext.Products.Add(product);
+            _menuManagerDbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var product = _menuManagerDbContext.Products
+                .SingleOrDefault(p => p.Id == id);
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+
+            var dbProduct = _menuManagerDbContext.Products
+                .SingleOrDefault(p => p.Id == product.Id);
+
+            if (dbProduct is null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            dbProduct.Name = product.Name;
+            dbProduct.Description = product.Description;
+            dbProduct.Price = product.Price;
+
+            _menuManagerDbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var product = _menuManagerDbContext.Products
+                .SingleOrDefault(p => p.Id == id);
+
+            return View(product);
+        }
+
+        //[HttpPost]
+        //[Route("Products/Delete/{id:int}")]
+        [HttpPost("Products/Delete/{id:int}")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            //var product = _menuManagerDbContext.Products
+            //    .SingleOrDefault(p => p.Id == id);
+
+            var product = new Product { Id = id, Name = string.Empty};
+
+            //if (product is null)
+            //{
+            //    return RedirectToAction("Index");
+            //}
+
+            _menuManagerDbContext.Products.Attach(product);
+
+            _menuManagerDbContext.Products.Remove(product);
+
+            _menuManagerDbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
